8ea8387116c38ec689f2520a1c72e41314ef88bd
DefoNotKarma <utkarshg74@gmail.com>
2024-04-15 00:39:32 +0530

    added multer and cloudinary functionalities

---
+++ src/utils/cloudinay.js
@@ -0,0 +1,38 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) 
+            return null;
+
+        // upload on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type : "auto"
+        })
+        // file has been uploaded
+
+        console.log("file has been uploaded on cloudinary : ",
+        response.url);
+
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+            //removes the locally saved temp file in server
+
+        return null;
+    }
+}
+
+
+export {uploadOnCloudinary} 
+  
