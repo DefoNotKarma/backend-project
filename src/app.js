66b83eb07e81cbc74e75ef967669026cc521125e
DefoNotKarma <utkarshg74@gmail.com>
2024-04-15 23:05:17 +0530

    configs, routes, controllers set up and running

--- src/app.js
+++ src/app.js
@@ -5,24 +5,31 @@ import cookieParser from "cookie-parser";
 
 const app = express()
 
+//configs
+
 app.use(cors({
     origin : process.env.CORS_ORIGIN,
     credentials : true
 }))
-
 app.use(express.json({
     limit : "16kb"
 }))
-
 app.use(express.urlencoded({
     extended : true,
     limit : "10kb"
 }))
-
 app.use(express.static("public"))
-
 app.use(cookieParser())
 
 
+//routes import
+
+import userRouter from './routes/user.routes.js';
+
+//router declaration
+
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:3000/api/v1/users/register
 
 export {app}
