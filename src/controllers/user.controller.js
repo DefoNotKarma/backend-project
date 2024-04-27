f2da3a1f587df54f96bf68e277b03993e415893d
DefoNotKarma <utkarshg74@gmail.com>
2024-04-27 23:59:44 +0530

    added pipelines for fetching subscriber data and sent as a response

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -393,6 +393,85 @@ const UpdateUserCover = asyncHandler(async (req, res) => {
 })
 
 
+const getUserChannel = asyncHandler(async (req, res) => {
+    const {username} = req.params
+
+    if(!username?.trim()){
+        throw new ApiError(400, "Channel not found")
+    }
+
+    const channel = User.aggregate([
+        {
+            $match : {
+                username : username?.toLowerCase()
+            }
+        },
+        {
+            $lookup : {
+                from : "subscriptions",
+                localField : "_id",
+                foreignField : "channel",
+                as : "subscribers"
+            }
+        },
+        {
+            $lookup : {
+                from : "subscriptions",
+                localField : "_id",
+                foreignField : "subscriber",
+                as : "subscribedTo"
+            }
+        },
+        {
+            $addFields : {
+                subscriberCount : {
+                    $size : "$subscribers"
+                },
+                channelsSubscribedToCount : {
+                    $size : "$subscribedTo"
+                },
+                isSubscribed : {
+                    $cond : {
+                        if : {$in : 
+                            [req.user?._id ,
+                             "$subscribers.subscriber"]
+                        },
+                        then : true,
+                        else : false
+                    }
+                }
+            }
+        },
+        {
+            $project : {
+                fullname : 1,
+                username : 1,
+                subscriberCount : 1,
+                channelsSubscribedToCount : 1,
+                isSubscribed : 1,
+                avatar : 1,
+                coverImage : 1,
+                email : 1
+            }
+        }
+
+    ])
+
+    console.log(channel)
+
+    if (!channel?.length){
+        throw new ApiError(404, "Channel does not exist")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, channel[0],
+            "User Channel fetched successfully")
+    )
+
+})
+
 
 export {
     registerUser,
